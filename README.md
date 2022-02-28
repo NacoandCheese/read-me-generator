9510a1a7590528b85ac9b29e0301fedcbd976895
Nicolo Danna <ndanna128@gmail.com>
2022-02-27 19:13:12 -0500

    test markdown

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
-# README.md Generator
-        ## Generates README.md
+
+    #README.md Generator
+    
+    ## Description
+        Generates README.md
+
+    ## Table of Contents
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+
+    ## Installation
+        Used npm inquirer npm install
+
+    ## Usage
+        To generate a professional README.md
+
+    ## License
+    ![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+    <br/>
+    This application is covered by the MIT license.
+
+    ## Contributing
+        
+
+    ## Tests
+        
+
+    ## Questions
+        Email me<br />
+    <br />
+
+    # Find me on GitHub: [NacoandCheese](https://github.com/NacoandCheese)<br />
+    <br />
+
+    # Email me with any questions: ndanna128@gmail.com<br /><br />
     
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,9 +15,47 @@ function generateMarkdown(data) {
     //Data is our answers to the users questions. Answers become data.
 
     // Return sends data out of the function. 
-    return `# ${data['projectTitle']}
-        ## ${data['description']}
-    `    
+    return `
+    #${data.projectTitle}
+    
+    ## Description
+        ${data.description}
+
+    ## Table of Contents
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+
+    ## Installation
+        ${data.installation}
+
+    ## Usage
+        ${data.usage}
+
+    ## License
+    ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+    <br/>
+    This application is covered by the ${data.license} license.
+
+    ## Contributing
+        ${data.contributing}
+
+    ## Tests
+        ${data.tests}
+
+    ## Questions
+        ${data.questions}<br />
+    <br />
+
+    # Find me on GitHub: [${data.username}](https://github.com/${data.username})<br />
+    <br />
+
+    # Email me with any questions: ${data.email}<br /><br />
+    `;    
 }
 
 module.exports = generateMarkdown;
